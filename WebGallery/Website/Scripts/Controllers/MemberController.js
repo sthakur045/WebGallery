33febc2ca48d2da9950f3e9f647ed1c28945cd47
Sanjay Thakur <sthakur045@hotmail.com>
2017-08-10 16:40:32 +0530

    First Commit

---
+++ WebGallery/Website/Scripts/Controllers/MemberController.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    angular.module("WebGallery")
+        .controller("MemberController", function ($scope, MemberService) {
+            $scope.members = MemberService.getMembers().then(function (result) {
+                $scope.members = result.data;
+            })
+        });
+})();
---
+++ WebGallery/Website/Scripts/Services/MemberService.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    angular.module("WebGallery")
+        .factory("MemberService", function ($http) {
+            return {
+                getMembers: function () {
+                    return $http.get("data.json").then(function (result) {
+                        return result;
+                    })
+                }
+            }
+        })
+})();
